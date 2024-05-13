b3086d7a2aea7e139cd5880c48c8a15cdf902bef
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-13 05:46:36 -0400

    se define create, y read del crud

--- database/consultas.js
+++ database/consultas.js
@@ -2,6 +2,32 @@ import dbase from "../config/db.js";
 const argumentos = process.argv.slice(2);
 const funcion = argumentos[0];
 
+const crearCancion = async ({ cancion, artista, tono }) => {
+    try { 
+        const query = `INSERT INTO canciones (cancion, artista, tono) VALUES ('${cancion}', '${artista}', '${tono}')`;
+        await dbase.query(query);
+    }
+    catch (error) {
+        console.log(" Se detecto el error", error);
+    }
+
+
+try {
+} catch (error) {
+  console.log(" Se detecto el error", error);
+}
+    
+};
+
+
+const leerCancion = async () => {
+  try {
+    const query = `SELECT * FROM canciones`;
+    await dbase.query(query);
+  } catch (error) {
+    console.log(" Se detecto el error", error);
+  }
+};
 
 
 //CRUD de canciones
---
+++ routes/routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+import path from "path";
+import dbase from "../config/db";
+const router = express.Router;
+const __dirname = import.meta.dirname;
