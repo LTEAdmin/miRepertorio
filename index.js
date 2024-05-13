b013dfe4670cee83a41ac79b05dfd4389690751c
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-13 05:57:51 -0400

    se levanta el servidor formato ES6

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import router from './routes/routes.js';
+const app=express();
+const port = process.env.PORT || 3000;
+
+//midelleware para rutas
+app.use('/', router);
+
+app.listen(port, console.log(`Example app listening on port http://localhost:$ {port}`));
